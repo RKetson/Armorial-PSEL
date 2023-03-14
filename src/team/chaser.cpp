be6a6f83172139074743c4bbce1f0fdd05d62497
RKetson <rennanketson@gmail.com>
2023-03-13 22:36:19 -0300

    Ajuste da posição do robô  e rotação para a bola

--- src/team/chaser.cpp
+++ src/team/chaser.cpp
@@ -1,10 +1,11 @@
 #include "chaser.h"
+#include <QtMath>
 #include <QDebug>
 
 void Chaser::run(){
 
     typedef enum{
-        ST_Search = 0, ST_Ajust, ST_Goal, ST_Back
+        ST_Search = 0, ST_Ajust, ST_Rotate, ST_Goal, ST_Back
     } States;
 
     static States state = ST_Search;
@@ -19,10 +20,14 @@ void Chaser::run(){
     wp->playingLeftSide() ? a = -1 : a = 1;
 
     QVector2D ball = wp->ballPosition();
+
+    float width_goal = wp->goalWidth();
     float width = wp->width();
     //Distância da parte superior até a bola
     float distY = width/2 - ball.y();
     float radius = wp->centerRadius();
+    QVector2D dir_Goal = (wp->theirGoalCenter() - ball) / (wp->theirGoalCenter() - ball).distanceToPoint(ball);
+
     //Distância da bola até o player
     float dist_PB[2] = {chaser->getPosition().x() - ball.x(), chaser->getPosition().y() - ball.y()};
 
@@ -31,7 +36,6 @@ void Chaser::run(){
 
         //Vai atrás da bola
         case ST_Search: {
-
             if(distY > width/2) go(chaser, ball + QVector2D(a * radius, radius));
             else if(distY < width/2) go(chaser, ball + QVector2D(a * radius, -radius));
             else{
@@ -47,13 +51,24 @@ void Chaser::run(){
         //Ajusta a posição do jogador antes do chute
         case ST_Ajust: {
 
-            float width_goal = wp->goalWidth();
-            float player_y = abs(chaser->getPosition().y());
+            float player_y = chaser->getPosition().y();
+            if((abs(player_y) < width_goal) && !(abs(player_y) < 0.06)){
+                qDebug() << "Teste";
+                go(chaser, ball - dir_Goal * radius * 0.6);
+                //rotate(chaser, ball);
+            }else state = ST_Rotate;
 
-            if((player_y < width_goal) && !(player_y < 0.06))
-                go(chaser, ball + QVector2D(a * radius, 0));
-            else state = ST_Goal;
+        break;
+        }
 
+        case ST_Rotate: {
+
+            float chaser_ori = tan(chaser->getOrientation());
+            float pb_ori = tan(dist_PB[1] / dist_PB[0]);
+
+            if(abs(chaser_ori) <= abs(pb_ori) * 1.2)
+                state = ST_Goal;
+            else rotate(chaser, ball);
         break;
         }
 
